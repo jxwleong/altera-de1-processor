71c7a99e0c16f6a07d3f9fda1f090ff36771a0ec
jxwleong <jason.xie.wei.leong@gmail.com>
2023-09-06 11:03:52 +0800

    Rename clk => clock as in the DUT verilog file is clock

--- test_microprocessor.py
+++ test_microprocessor.py
@@ -6,9 +6,9 @@ async def generate_clock(dut):
     """Generate clock pulses."""
 
     for cycle in range(10):
-        dut.clk.value = 0
+        dut.clock.value = 0
         await Timer(1, units="ns")
-        dut.clk.value = 1
+        dut.clock.value = 1
         await Timer(1, units="ns")
 
 
@@ -18,7 +18,7 @@ async def test(dut):
     await cocotb.start(generate_clock(dut))  # run the clock "in the background"
 
     await Timer(5, units="ns")  # wait a bit
-    await FallingEdge(dut.clk)  # wait for falling edge/"negedge"
+    await FallingEdge(dut.clock)  # wait for falling edge/"negedge"
 
-    dut._log.info("my_signal_1 is %s", dut.clk.value)
+    dut._log.info("my_signal_1 is %s", dut.clock.value)
     #assert dut.my_signal_2.value[0] == 0, "my_signal_2[0] is not 0!"
